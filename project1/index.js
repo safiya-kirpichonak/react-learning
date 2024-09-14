682f2a450b7cee980d29fc2900e650819364d6d7
Sofia K. <sophiakirpichenok@gmail.com>
2024-09-14 19:20:00 +0200

    completed 3 tasks

---
+++ project1/index.js
@@ -0,0 +1,43 @@
+// task 1
+
+// ReactDOM.render(
+//   <div>
+//     <p>Products that I need to buy:</p>
+//     <ul>
+//       <li>12 eggs</li>
+//       <li>3 applies</li>
+//       <li>milk</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// task 2
+
+// function NavBar() {
+//   return (
+//     <div>
+//       <a>link 1</a>
+//       <a>link 2</a>
+//       <a>link 3</a>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<NavBar />, document.getElementById("root"));
+
+// task 3
+
+// imperative way:
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "Hello, React!";
+// h1.className = "header";
+// document.getElementById("root").appendChild(h1);
+
+// declarative way:
+
+// ReactDOM.render(
+//   <h1 class="header">Hello, React!</h1>,
+//   document.getElementById("root")
+// );
