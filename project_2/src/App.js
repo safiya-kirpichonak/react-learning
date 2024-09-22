428fcc63cc9c6711ae8cfead0cfea2c2fcb26435
Sofia K. <sophiakirpichenok@gmail.com>
2024-09-22 23:26:23 +0200

    started project_2

---
+++ project_2/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Nav from "./components/Nav.js";
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
---
+++ project_2/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logo from "../images/logo.svg";
+
+export default function Nav() {
+  return (
+    <nav>
+      <img src={logo} alt="logo" />
+    </nav>
+  );
+}
