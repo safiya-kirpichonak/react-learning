20502deb0eeda6c4f764a90bd02db7adcbc30fe2
Sofia K. <sophiakirpichenok@gmail.com>
2024-09-26 19:25:38 +0200

    doing project_2

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -4,12 +4,21 @@ import Nav from "./components/Nav.js";
 import Card from "./components/Card.js";
 import Hero from "./components/Hero.js";
 
+import card1 from "./images/card1-photo.png";
+
 export default function App() {
   return (
     <div>
       <Nav />
-      {/* <Hero /> */}
-      <Card />
+      <Hero />
+      <Card
+        img={card1}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- project_2/src/components/Card.js
+++ project_2/src/components/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import star from "../images/star.svg";
-import cardPhoto from "../images/card1-photo.png";
 
 export default function Card({
   img,
@@ -13,16 +12,16 @@ export default function Card({
 }) {
   return (
     <div className="card">
-      <img src={cardPhoto} alt="card-photo" />
+      <img src={img} alt="card-photo" />
       <div className="card--rating">
         <img src={star} alt="star" className="card--rating-logo" />
-        <span>5.0</span>
-        <span className="grey-text">(6)</span>
-        <span className="grey-text"> USA</span>
+        <span>{rating}</span>
+        <span className="grey-text">({reviewCount})</span>
+        <span className="grey-text"> {country}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
+      <p>{title}</p>
       <p>
-        <span className="bold-text">From $136</span> / person
+        <span className="bold-text">From ${price}</span> / person
       </p>
     </div>
   );
