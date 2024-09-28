80a4fb64929159a6b22b53d50d30a20bbd7e788d
Sofia K. <sophiakirpichenok@gmail.com>
2024-09-28 20:10:05 +0200

    completed project_2

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 
+import cardsData from "./cardsData.js";
+
 import Nav from "./components/Nav.js";
 import Card from "./components/Card.js";
 import Hero from "./components/Hero.js";
 
 export default function App() {
+  const cards = cardsData.map((item) => <Card key={item.id} item={item} />);
+
   return (
     <div>
       <Nav />
       <Hero />
-      <Card
-        img="card1-photo.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
--- project_2/src/components/Card.js
+++ project_2/src/components/Card.js
@@ -1,25 +1,26 @@
 import React from "react";
 
-export default function Card({
-  img,
-  rating,
-  reviewCount,
-  country,
-  title,
-  price,
-}) {
+export default function Card({ item }) {
+  const badgeText =
+    item.locale === "Online"
+      ? "ONLINE"
+      : item.openSpots === 0
+      ? "SOLD OUT"
+      : null;
+
   return (
     <div className="card">
-      <img src={`./images/${img}`} alt="card-photo" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`./images/${item.coverImg}`} alt="card-photo" />
       <div className="card--rating">
         <img src="./images/star.svg" alt="star" className="card--rating-logo" />
-        <span>{rating}</span>
-        <span className="grey-text">({reviewCount})</span>
-        <span className="grey-text"> {country}</span>
+        <span>{item.stats.rating}</span>
+        <span className="grey-text">({item.stats.reviewCount})</span>
+        <span className="grey-text"> {item.locale}</span>
       </div>
-      <p>{title}</p>
+      <p>{item.title}</p>
       <p>
-        <span className="bold-text">From ${price}</span> / person
+        <span className="bold-text">From ${item.price}</span> / person
       </p>
     </div>
   );
