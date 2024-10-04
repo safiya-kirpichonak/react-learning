cd97efcc2d41f5c3f26ab3973116c71339095985
Sofia K. <sophiakirpichenok@gmail.com>
2024-10-04 22:52:07 +0200

    doing task_2

--- tasks_2/index.js
+++ tasks_2/index.js
@@ -91,50 +91,203 @@
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-// task 5
+// task "Jokes"
 
-const jokes = [
-  {
-    setup: "I got my daughter a fridge for her birthday.",
-    punchline: "I can't wait to see her face light up when she opens it.",
-  },
-  {
-    setup: "How did the hacker escape the police?",
-    punchline: "He just ransomware!",
-  },
-  {
-    setup: "Why don't pirates travel on mountain roads?",
-    punchline: "Scurvy.",
-  },
-  {
-    setup: "Why do bees stay in the hive in the winter?",
-    punchline: "Swarm.",
-  },
-  {
-    setup: "What's the best thing about Switzerland?",
-    punchline: "I don't know, but the flag is a big plus!",
-  },
-];
+// const jokes = [
+//   {
+//     setup: "I got my daughter a fridge for her birthday.",
+//     punchline: "I can't wait to see her face light up when she opens it.",
+//   },
+//   {
+//     setup: "How did the hacker escape the police?",
+//     punchline: "He just ransomware!",
+//   },
+//   {
+//     setup: "Why don't pirates travel on mountain roads?",
+//     punchline: "Scurvy.",
+//   },
+//   {
+//     setup: "Why do bees stay in the hive in the winter?",
+//     punchline: "Swarm.",
+//   },
+//   {
+//     setup: "What's the best thing about Switzerland?",
+//     punchline: "I don't know, but the flag is a big plus!",
+//   },
+// ];
 
-function Joke({ setup, punchline }) {
+// function Joke({ setup, punchline }) {
+//   const [isShown, useIsShown] = React.useState(false);
+//   const handlerClick = () => useIsShown((oldValue) => !oldValue);
+
+//   return (
+//     <div>
+//       <p style={{ color: "green" }}>
+//         <b>{setup}</b>
+//       </p>
+//       <p style={{ color: "red" }}>{isShown && <b>{punchline}</b>}</p>
+//       <button onClick={handlerClick}>
+//         {isShown ? "Hide" : "Show"} punchline
+//       </button>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const jokeElements = jokes.map((joke) => (
+//     <Joke setup={joke.setup} punchline={joke.punchline} />
+//   ));
+
+//   return <div>{jokeElements}</div>;
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// task "Messages"
+
+// function App() {
+//   const [messages] = React.useState([]);
+
+//   return (
+//     <div>
+//       {messages.length === 0 ? (
+//         <h1>You are all caught up!</h1>
+//       ) : (
+//         <h1>
+//           You have {messages.length} unread{" "}
+//           {messages.length === 1 ? "message" : "messages"}!
+//         </h1>
+//       )}
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// task "Form"
+
+function Form() {
+  const [formData, setFormData] = React.useState({
+    employmentStatus: "",
+    isFriendly: true,
+    firstName: "",
+    lastName: "",
+    favColor: "",
+    comment: "",
+    email: "",
+  });
+
+  console.log(formData);
+
+  function handleChange(event) {
+    const { value, name, type, checked } = event.target;
+    setFormData((oldFormData) => ({
+      ...oldFormData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
   return (
-    <div>
-      <p style={{ color: "green" }}>
-        <b>{setup}</b>
-      </p>
-      <p style={{ color: "red" }}>
-        <b>{punchline}</b>
-      </p>
-    </div>
+    <form>
+      <input
+        type="text"
+        placeholder="First name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Last name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <br />
+
+      <textarea
+        placeholder="Comment"
+        onChange={handleChange}
+        name="comment"
+        value={formData.comment}
+      />
+      <br />
+
+      <input
+        type="checkbox"
+        id="isFriendly"
+        name="isFriendly"
+        onChange={handleChange}
+        checked={formData.isFriendly}
+      />
+      <label htmlFor="isFriendly">Are you friendly?</label>
+      <br />
+      <br />
+
+      <fieldset>
+        <legend>Current employment status:</legend>
+        <input
+          type="radio"
+          id="unemployed"
+          name="employmentStatus"
+          value="unemployed"
+          onChange={handleChange}
+          checked={formData.employmentStatus === "unemployed"}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+        <input
+          type="radio"
+          id="part-time"
+          name="employmentStatus"
+          value="part-time"
+          onChange={handleChange}
+          checked={formData.employmentStatus === "part-time"}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+        <input
+          type="radio"
+          id="full-time"
+          name="employmentStatus"
+          value="full-time"
+          checked={formData.employmentStatus === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+      </fieldset>
+      <br />
+
+      <label htmlFor="favColor">What is your favorite color?</label>
+      <br />
+      <select
+        id="favColor"
+        name="favColor"
+        onChange={handleChange}
+        value={formData.favColor}
+      >
+        <option value="">-- Chose --</option>
+        <option value="red">Red</option>
+        <option value="white">White</option>
+        <option value="black">Black</option>
+      </select>
+      <br />
+      <br />
+
+      <button></button>
+    </form>
   );
 }
 
-function App() {
-  const jokeElements = jokes.map((joke) => (
-    <Joke setup={joke.setup} punchline={joke.punchline} />
-  ));
-
-  return <div>{jokeElements}</div>;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Form />, document.getElementById("root"));
