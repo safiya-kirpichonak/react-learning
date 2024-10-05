a88d277fff36554ee951fa623bdbac84c586b833
Sofia K. <sophiakirpichenok@gmail.com>
2024-10-05 10:51:13 +0200

    doing task_3

--- tasks_3/index.js
+++ tasks_3/index.js
@@ -282,17 +282,6 @@
 // task "Submit form"
 
 function App() {
-  /**
-   * Challenge: Connect the form to local state
-   *
-   * 3. When the user clicks "Sign up", check if the
-   *    password & confirmation match each other. If
-   *    so, log "Successfully signed up" to the console.
-   *    If not, log "passwords to not match" to the console.
-   * 4. Also when submitting the form, if the person checked
-   *    the "newsletter" checkbox, log "Thanks for signing
-   *    up for our newsletter!" to the console.
-   */
   const [formData, setFormData] = React.useState({
     email: "",
     password: "",
